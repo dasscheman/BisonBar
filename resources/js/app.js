11f3c0985b4e27d65a9e2acad64732e38ba748f8
Daan Asscheman <dasscheman@gmail.com>
2025-08-05 17:03:43 +0200

    modal not hiding.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,14 @@ window.bootstrap = bootstrap;
 
 
 import './soft-ui-dashboard';
+
+window.addEventListener('hideModal', event => {
+    const modalElements = document.getElementsByClassName('modal');
+    for (let i = 0; i < modalElements.length; i++) {
+        const element = modalElements[i];
+        // Perform actions on each element
+        var myModal = bootstrap.Modal.getOrCreateInstance(element);
+        myModal.hide();
+        console.log(element);
+    }
+})
