9b7097c9ea1f084ac435e094c045bdf516ed025c
Daan Asscheman <dasscheman@gmail.com>
2025-08-05 17:01:24 +0200

    Foutje cron

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Schedule::command('backup:clean')->daily()->at('01:00');
-Schedule::command('backup:run')->daily()->at('01:30');
-Schedule::command('backup:monitor')->daily()->at('03:00');
+Schedule::command('backup:clean')->dailyAt('01:00');
+Schedule::command('backup:run')->dailyAt('01:30');
+Schedule::command('backup:monitor')->dailyAt('03:00');
