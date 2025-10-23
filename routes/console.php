a80ba2160f2ab0259cd8cef8bcc57fef907ba1da
Daan Asscheman <dasscheman@gmail.com>
2025-10-23 16:32:40 +0200

    Check on existing user.

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schedule;
 
 $serviceUser = User::where('email', config('mail.from.address'))->first();
-Auth::loginUsingId($serviceUser->id, true);
-Schedule::command('backup:clean')->dailyAt('01:00');
-Schedule::command('backup:run')->dailyAt('01:30');
-Schedule::command('backup:monitor')->dailyAt('03:00');
-Auth::logout();
+if($serviceUser) {
+    Auth::loginUsingId($serviceUser->id, true);
+    Schedule::command('backup:clean')->dailyAt('01:00');
+    Schedule::command('backup:run')->dailyAt('01:30');
+    Schedule::command('backup:monitor')->dailyAt('03:00');
+    Auth::logout();
+}
