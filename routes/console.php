af3e0304c1f7dd701f29ba17a637e97a1d5a641a
Daan Asscheman <dasscheman@gmail.com>
2025-10-23 16:24:39 +0200

    removes info call.

--- routes/console.php
+++ routes/console.php
@@ -5,9 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schedule;
 
 $serviceUser = User::where('email', config('mail.from.address'))->first();
-$this->info('Login user ' . $serviceUser->id);
 Auth::loginUsingId($serviceUser->id, true);
-
 Schedule::command('backup:clean')->dailyAt('01:00');
 Schedule::command('backup:run')->dailyAt('01:30');
 Schedule::command('backup:monitor')->dailyAt('03:00');
